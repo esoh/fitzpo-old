fe8b5566a13997bda860afe42b1dfae4b8113fc4
Sean Oh <sean.so.oh@gmail.com>
2018-09-19 19:06:57 -0700

    Add homepage response

--- app.js
+++ app.js
@@ -8,7 +8,14 @@ const mongoose = require('mongoose');
 const app = express(); // create an express application
 const port = 8080;// use whatever port to test
 
+// route http: / to the callback function defined below.
+// This function is called when a GET http request to "/" is made.
+app.get('/', (req, res) => {
+   res.send('Invalid Endpoint');
+});
+
 // () => {//code here} is a callback function that is called inside listen.
+// You can also replace it with function(){//code here}
 app.listen(port, () => {
    console.log('Server started on port ' + port);
 });
