eca6d5d630fad35f5b9362ea108bfe92561cad42
Sean Oh <sean.so.oh@gmail.com>
2018-09-19 21:46:14 -0700

    Add user find functions

--- models/user.js
+++ models/user.js
@@ -22,3 +22,12 @@ const UserSchema = mongoose.Schema({
 });
 
 const User = module.exports = mongoose.model('User', UserSchema);
+
+module.exports.getUserById = function(id, callback){
+   User.findById(id, callback);
+}
+
+module.exports.getUserByUsername = function(username, callback){
+   const query = {username: username}
+   User.findOne(query, callback);
+}
