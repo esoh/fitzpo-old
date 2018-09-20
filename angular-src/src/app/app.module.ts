c1fe6c1d8ae85f9b5abdfc571be00e56c9637733
Sean Oh <sean.so.oh@gmail.com>
2018-09-20 15:36:11 -0700

    Create dashboard, login, profile, and signup components

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -3,11 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    DashboardComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule
