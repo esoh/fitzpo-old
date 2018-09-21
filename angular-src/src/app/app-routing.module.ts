c84889a26104bb717032759e7ed7d8672c41f593
Sean Oh <sean.so.oh@gmail.com>
2018-09-21 12:32:04 -0700

    Add routing with home page

--- angular-src/src/app/app-routing.module.ts
+++ angular-src/src/app/app-routing.module.ts
@@ -1,7 +1,34 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+   // map home to home component
+   {
+      path: '',
+      component: HomeComponent
+   },
+   {
+      path: 'signup',
+      component: SignupComponent
+   },
+   {
+      path: 'login',
+      component: LoginComponent
+   },
+   {
+      path: 'dashboard',
+      component: DashboardComponent
+   },
+   {
+      path: 'profile',
+      component: ProfileComponent
+   }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { ProfileComponent } from './components/profile/profile.component';
     LoginComponent,
     SignupComponent,
     DashboardComponent,
-    ProfileComponent
+    ProfileComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
