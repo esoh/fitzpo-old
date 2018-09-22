1eaae4fd072f2742527ce241265fe9a134bf89fb
Sean Oh <sean.so.oh@gmail.com>
2018-09-22 14:41:19 -0700

    Add Signup frontend form

--- angular-src/src/app/components/signup/signup.component.ts
+++ angular-src/src/app/components/signup/signup.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-signup',
-  templateUrl: './signup.component.html',
-  styleUrls: ['./signup.component.css']
+   selector: 'app-signup',
+   templateUrl: './signup.component.html',
+   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+   name: String;
+   username: String;
+   email: String;
+   password: String;
 
-  ngOnInit() {
-  }
+   constructor() { }
+
+   ngOnInit() {
+   }
+
+   onRegisterSubmit() {
+      const user = {
+         name: this.name,
+         username: this.username,
+         email: this.email,
+         password: this.password
+      }
+   }
 
 }
