4b4cfa6cee15b186329708e3a84474479e2c7eab
Sean Oh <sean.so.oh@gmail.com>
2018-09-28 14:31:23 -0700

    Remove unnecessary passport require bc of reorganization

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,6 @@
 
 const express = require('express');
 const router = express.Router();
-const passport = require('passport');
 const jwt = require('jsonwebtoken');
 const config = require('../config/database');
 const User = require('../models/user');
