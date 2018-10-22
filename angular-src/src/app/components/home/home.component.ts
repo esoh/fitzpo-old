221c083bc408e20b81130b1eca3f11be9db45785
Sean Oh <sean.so.oh@gmail.com>
2018-10-21 22:15:10 -0700

    Fix home signup/login links from router page links to modals

--- angular-src/src/app/components/home/home.component.ts
+++ angular-src/src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { EntryModalService } from '../../services/entry-modal.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+   constructor(public modalService: EntryModalService) { }
 
-  ngOnInit() {
-  }
+   ngOnInit() {
+   }
 
 }
