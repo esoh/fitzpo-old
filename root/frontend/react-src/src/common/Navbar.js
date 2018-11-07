11248b7b365cccd3d1d8459b1853ce783496f240
Sean Oh <sean.so.oh@gmail.com>
2018-11-07 02:09:35 -0800

    Style navbar with bootstrap cdn

--- root/frontend/react-src/src/common/Navbar.js
+++ root/frontend/react-src/src/common/Navbar.js
@@ -3,7 +3,50 @@ import React from 'react';
 class Navbar extends React.Component {
     render() {
         return (
-            <div> Navbar </div>
+            <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
+                <div class="container">
+
+                    {/* Brand */}
+                    <a class="navbar-brand">GymMate</a>
+
+                    {/* Collapse toggler button */}
+                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarDefault">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+
+                    {/* Collapsible navbar items */}
+                    <div class="collapse navbar-collapse" id="navbarDefault">
+                        <ul class="navbar-nav ml-auto">
+
+                            <li class="nav-item" routerLinkActive='active'>
+                                <a class="nav-link">Home</a>
+                            </li>
+
+                            <li class="nav-item" routerLinkActive='active'>
+                                <a class="nav-link">Programs</a>
+                            </li>
+
+                            <li class="nav-item" routerLinkActive='active'>
+                                <a class="nav-link">Exercises</a>
+                            </li>
+
+                            <li class="nav-item" routerLinkActive='active'>
+                                <a class="nav-link">Profile</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link">Sign up</a>
+                            </li>
+
+                            <li class="nav-item">
+                                <a class="nav-link">Log in</a>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav>
         );
     }
 }
