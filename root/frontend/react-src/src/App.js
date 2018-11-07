b6a4d01fca017844451546532012c835dc5e0b04
Sean Oh <sean.so.oh@gmail.com>
2018-11-07 01:37:45 -0800

    Create Navbar component

--- root/frontend/react-src/src/App.js
+++ root/frontend/react-src/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import Navbar from './common/Navbar'
 
 class App extends Component {
     render() {
         return (
             <div className="App">
+                <Navbar/>
                 <h1>Hello World</h1>
             </div>
         );
