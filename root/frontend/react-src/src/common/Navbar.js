5ee35f148072439bea066351285435991a15567e
Sean Oh <sean.so.oh@gmail.com>
2018-11-08 03:02:06 -0800

    Fix class attributes to JSX className in Navbar

--- root/frontend/react-src/src/common/Navbar.js
+++ root/frontend/react-src/src/common/Navbar.js
@@ -4,44 +4,44 @@ import { NavLink, Link } from 'react-router-dom';
 class Navbar extends React.Component {
     render() {
         return (
-            <nav class="navbar navbar-expand-md navbar-light bg-light fixed-top">
-                <div class="container">
+            <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
+                <div className="container">
 
                     {/* Brand */}
                     <Link className="navbar-brand" to="/">GymMate</Link>
 
                     {/* Collapse toggler button */}
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
+                    <button className="navbar-toggler" type="button" data-toggle="collapse"
                         data-target="#navbarDefault">
-                        <span class="navbar-toggler-icon"></span>
+                        <span className="navbar-toggler-icon"></span>
                     </button>
 
                     {/* Collapsible navbar items */}
-                    <div class="collapse navbar-collapse" id="navbarDefault">
-                        <ul class="navbar-nav ml-auto">
+                    <div className="collapse navbar-collapse" id="navbarDefault">
+                        <ul className="navbar-nav ml-auto">
 
-                            <li class="nav-item">
-                                <NavLink className="nav-link" activeClassName="active" exact to="/">Home</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeclass="active" exact to="/">Home</NavLink>
                             </li>
 
-                            <li class="nav-item">
-                                <NavLink className="nav-link" activeClassName="active" to="/programs">Programs</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeclass="active" to="/programs">Programs</NavLink>
                             </li>
 
-                            <li class="nav-item">
-                                <NavLink className="nav-link" activeClassName="active" to="/exercises">Exercises</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeclass="active" to="/exercises">Exercises</NavLink>
                             </li>
 
-                            <li class="nav-item">
-                                <NavLink className="nav-link" activeClassName="active" to="/profile">Profile</NavLink>
+                            <li className="nav-item">
+                                <NavLink className="nav-link" activeclass="active" to="/profile">Profile</NavLink>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link">Sign up</a>
+                            <li className="nav-item">
+                                <a className="nav-link">Sign up</a>
                             </li>
 
-                            <li class="nav-item">
-                                <a class="nav-link">Log in</a>
+                            <li className="nav-item">
+                                <a className="nav-link">Log in</a>
                             </li>
 
                         </ul>
