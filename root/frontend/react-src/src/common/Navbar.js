d571983075e84eb1d5d41a43c9f1a0f4d10e89ad
Sean Oh <sean.so.oh@gmail.com>
2018-11-09 04:02:42 -0800

    Collapse navbar after nav item is selected

--- root/frontend/react-src/src/common/Navbar.js
+++ root/frontend/react-src/src/common/Navbar.js
@@ -4,15 +4,22 @@ import { NavLink, Link } from 'react-router-dom';
 class Navbar extends React.Component {
     constructor(props){
         super(props);
-        this.toggleNavbar = this.toggleNavbar.bind(this);
         this.state = {
             collapsed: true
+            this.toggleNavbar = this.toggleNavbar.bind(this);
+            this.collapseNavbar = this.collapseNavbar.bind(this);
         };
     }
 
     toggleNavbar() {
         this.setState({
             collapsed: !this.state.collapsed
+        });
+    }
+
+    collapseNavbar() {
+        this.setState({
+            collapsed: true
         });
     }
 
@@ -37,33 +44,27 @@ class Navbar extends React.Component {
                     <div className={collapsibleNavbarClass}>
                         <ul className="navbar-nav ml-auto">
 
-                            <li className="nav-item" data-toggle="collapse"
-                                data-target="#navbarDefault">
+                            <li className="nav-item" onClick={this.collapseNavbar}>
                                 <NavLink className="nav-link" exact to="/" >Home</NavLink>
                             </li>
 
-                            <li className="nav-item" data-toggle="collapse"
-                                data-target="#navbarDefault">
+                            <li className="nav-item" onClick={this.collapseNavbar}>
                                 <NavLink className="nav-link" to="/programs">Programs</NavLink>
                             </li>
 
-                            <li className="nav-item" data-toggle="collapse"
-                                data-target="#navbarDefault">
+                            <li className="nav-item" onClick={this.collapseNavbar}>
                                 <NavLink className="nav-link" to="/exercises">Exercises</NavLink>
                             </li>
 
-                            <li className="nav-item" data-toggle="collapse"
-                                data-target="#navbarDefault">
+                            <li className="nav-item" onClick={this.collapseNavbar}>
                                 <NavLink className="nav-link" to="/profile">Profile</NavLink>
                             </li>
 
-                            <li className="nav-item" data-toggle="collapse"
-                                data-target="#navbarDefault">
+                            <li className="nav-item" onClick={this.collapseNavbar}>
                                 <a className="nav-link">Sign up</a>
                             </li>
 
-                            <li className="nav-item" data-toggle="collapse"
-                                data-target="#navbarDefault">
+                            <li className="nav-item" onClick={this.collapseNavbar}>
                                 <a className="nav-link">Log in</a>
                             </li>
 
