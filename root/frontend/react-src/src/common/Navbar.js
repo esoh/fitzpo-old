b39a4351b8b6a8afafbc30e3a88392ee509a2b55
Sean Oh <sean.so.oh@gmail.com>
2018-11-14 01:03:29 -0800

    Add transition animation to navbar hamgurger menu collapsing

--- root/frontend/react-src/src/common/Navbar.js
+++ root/frontend/react-src/src/common/Navbar.js
@@ -1,5 +1,24 @@
 import React from 'react';
 import { NavLink, Link } from 'react-router-dom';
+import css from 'dom-helpers/style';
+import CSSTransition from 'react-transition-group/CSSTransition';
+
+const duration = 300;
+const collapsibleNavClassNames={
+    enter: 'collapsing navbar-collapse',
+    enterDone: 'collapse navbar-collapse show',
+    exit: 'collapsing navbar-collapse',
+    exitDone: 'collapse navbar-collapse',
+};
+
+function getHeightValue(elem) {
+    return (
+        elem['offsetHeight'] +
+        parseInt(css(elem, 'marginTop'), 10) +
+        parseInt(css(elem, 'marginBottom'), 10)
+    );
+}
+
 
 class Navbar extends React.Component {
     constructor(props){
@@ -23,10 +42,32 @@ class Navbar extends React.Component {
         });
     }
 
+    /* Handle css element heights for bootstrap.css animations to apply */
+    // TODO: FIX COLLAPSING ANIMATION
+    onEnter = elem => {
+        elem.style['height'] = '0';
+    };
+
+    onEntering = elem => {
+        elem.style['height'] = `${elem['scrollHeight']}px`;
+    };
+
+    onEntered = elem => {
+        elem.style['height'] = null;
+    };
+
+    onExit = elem => {
+        elem.style['height'] = `${getHeightValue(elem)}px`;
+        console.log(elem.style['height']);
+    };
+
+    onExiting = elem => {
+        elem.style['height'] = '0';
+    };
+
     render() {
         const collapsed = this.state.collapsed;
-        const collapsibleNavbarClass = collapsed ? 'collapse navbar-collapse' : 'collapse navbar-collapse show';
-        const collapsibleNavbarTogglerClass = collapsed ? 'navbar-toggler collapsed' : 'navbar-toggler'
+        const collapsibleNavbarTogglerClass = collapsed ? 'navbar-toggler collapsed' : 'navbar-toggler';
 
         return (
             <nav className="navbar navbar-expand-md navbar-light bg-light fixed-top">
@@ -41,35 +82,49 @@ class Navbar extends React.Component {
                     </button>
 
                     {/* Collapsible navbar items */}
-                    <div className={collapsibleNavbarClass}>
-                        <ul className="navbar-nav ml-auto">
+                    <CSSTransition
+                        in={!collapsed}
+                        timeout={duration}
+                        classNames={collapsibleNavClassNames}
+                        onEnter={this.onEnter}
+                        onEntering={this.onEntering}
+                        onEntered={this.onEntered}
+                        onExit={this.onExit}
+                        onExiting={this.onExiting}
+                    >
+                        {state => { return(
+                            <div className="collapse navbar-collapse">
+                            {console.log(state)}
+                                <ul className="navbar-nav ml-auto">
 
-                            <li className="nav-item" onClick={this.collapseNavbar}>
-                                <NavLink className="nav-link" exact to="/" >Home</NavLink>
-                            </li>
+                                    <li className="nav-item" onClick={this.collapseNavbar}>
+                                        <NavLink className="nav-link" exact to="/">Home</NavLink>
+                                    </li>
 
-                            <li className="nav-item" onClick={this.collapseNavbar}>
-                                <NavLink className="nav-link" to="/programs">Programs</NavLink>
-                            </li>
+                                    <li className="nav-item" onClick={this.collapseNavbar}>
+                                        <NavLink className="nav-link" to="/programs">Programs</NavLink>
+                                    </li>
 
-                            <li className="nav-item" onClick={this.collapseNavbar}>
-                                <NavLink className="nav-link" to="/exercises">Exercises</NavLink>
-                            </li>
+                                    <li className="nav-item" onClick={this.collapseNavbar}>
+                                        <NavLink className="nav-link" to="/exercises">Exercises</NavLink>
+                                    </li>
 
-                            <li className="nav-item" onClick={this.collapseNavbar}>
-                                <NavLink className="nav-link" to="/profile">Profile</NavLink>
-                            </li>
+                                    <li className="nav-item" onClick={this.collapseNavbar}>
+                                        <NavLink className="nav-link" to="/profile">Profile</NavLink>
+                                    </li>
 
-                            <li className="nav-item" onClick={this.collapseNavbar}>
-                                <a className="nav-link">Sign up</a>
-                            </li>
+                                    <li className="nav-item" onClick={this.collapseNavbar}>
+                                        <a className="nav-link">Sign up</a>
+                                    </li>
 
-                            <li className="nav-item" onClick={this.collapseNavbar}>
-                                <a className="nav-link">Log in</a>
-                            </li>
+                                    <li className="nav-item" onClick={this.collapseNavbar}>
+                                        <a className="nav-link">Log in</a>
+                                    </li>
 
-                        </ul>
-                    </div>
+                                </ul>
+                            </div>
+                        );}}
+                    </CSSTransition>
                 </div>
             </nav>
         );
