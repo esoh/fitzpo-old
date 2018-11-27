8994f91e1339426f6aab0b365cd1d6f5f36c428f
Sean Oh <sean.so.oh@gmail.com>
2018-11-27 13:19:49 -0800

    Separate navbar animation functionality from content into NavbarCollapse

---
+++ root/frontend/react-src/src/common/NavbarCollapse.js
@@ -0,0 +1,97 @@
+import React from 'react';
+import Transition, {
+    EXITED,
+    ENTERED,
+    ENTERING,
+    EXITING,
+} from 'react-transition-group/Transition';
+
+const duration = 350;
+const collapseStyles = {
+    [EXITED]: {
+        display: 'none',
+    },
+    [EXITING]: {
+        position: 'relative',
+        overflow: 'hidden',
+        transition: `height ${duration}ms ease`,
+    },
+    [ENTERING]: {
+        position: 'relative',
+        overflow: 'hidden',
+        transition: `height ${duration}ms ease`,
+    },
+    [ENTERED]: {
+        display: 'block',
+    },
+}
+
+function getExpandWidthValue(expandWidthType){
+    switch(expandWidthType){
+        case 'sm':
+            return 576;
+        case 'md':
+            return 768;
+        case 'lg':
+            return 992;
+        case 'xl':
+            return 1200;
+        default:
+            return undefined;
+    }
+}
+
+class NavbarCollapse extends React.Component {
+
+    /* Handle css element heights for animations to adjust heights*/
+    onEnter = elem => {
+        elem.style['height'] = '0';
+    };
+
+    onEntering = elem => {
+        elem.style['height'] = `${elem['scrollHeight']}px`;
+        this.forceReflow(elem);
+    };
+
+    onEntered = elem => {
+        elem.style['height'] = null;
+    };
+
+    onExit = elem => {
+        elem.style['height'] = `${elem['offsetHeight']}px`;
+        this.forceReflow(elem);
+    };
+
+    onExiting = elem => {
+        // prevent closing animation from closing to 0 when navbar is expanded
+        if(!this.props.expand || window.innerWidth < getExpandWidthValue(this.props.expand)){
+            elem.style['height'] = '0';
+        }
+    };
+
+    forceReflow(node) {
+        node.offsetHeight; // eslint-disable-line no-unused-expressions
+    }
+
+    render() {
+        return (
+            <Transition
+                in={this.props.in}
+                timeout={duration}
+                onEnter={this.onEnter}
+                onEntering={this.onEntering}
+                onEntered={this.onEntered}
+                onExit={this.onExit}
+                onExiting={this.onExiting}
+            >
+                {state => { return(
+                    <div className='navbar-collapse' style={{...collapseStyles[state]}}>
+                        {this.props.children}
+                    </div>
+                ) }}
+            </Transition>
+        )
+    }
+}
+
+export default NavbarCollapse;
