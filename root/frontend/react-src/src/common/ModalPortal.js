581553726aeaa8475e3be9d8e598a2b5fbbb3315
Sean Oh <sean.so.oh@gmail.com>
2018-11-29 12:20:36 -0800

    Add clarifying comments to ReactDOM Portal usage

--- root/frontend/react-src/src/common/ModalPortal.js
+++ root/frontend/react-src/src/common/ModalPortal.js
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom';
 
 import './Modal.css'
 
+// since 3rd party apps like to render to body, we don't want to render to body.
+// Everything renders to body and if we render to body, it could break our app.
+// We want to render to a div that is a child of body (root).
+
 const appRoot = document.getElementById('root');
 
 class ModalPortal extends React.Component {
