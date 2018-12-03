4328204a944f25c22a067383281d44b5e4a3393e
Logan Rogers <lrxcountry@gmail.com>
2018-12-03 01:55:53 -0800

    implement signup clent side form validation

--- root/frontend/react-src/src/auth/Signup.js
+++ root/frontend/react-src/src/auth/Signup.js
@@ -7,8 +7,77 @@ class Signup extends React.Component {
     constructor() {
         super();
         this.state = {
+            isValidated: false,
+            userValid: true,
+            emailValid: true,
+            pwValid: true
         };
     }
+    validate = () => {
+        const form = document.getElementById("entry-form");
+        const formLength = form.length;
+        if (form.checkValidity() === false) {
+            for (let i = 0; i < formLength; i++) {
+                const elem = form[i];
+                // const errorLabel = elem.parentNode.querySelector(".invalid-feedback");
+                if (/*errorLabel && */elem.nodeName.toLowerCase() !== "button") {
+                    if (!elem.validity.valid) {
+                        console.log(elem.id);
+                        if (elem.id === "user-field") {
+                            this.setState({
+                                userValid: false
+                            });
+                        } else if (elem.id === "pw-field") {
+                            this.setState( {
+                                pwValid:  false
+                            });
+                        } else if (elem.id === "email-field") {
+                            this.setState( {
+                                emailValid: false
+                            })
+                        }
+                    } else {
+                        if (elem.id === "user-field") {
+                            this.setState({
+                                userValid: true
+                            });
+                        } else if (elem.id === "pw-field") {
+                            this.setState( {
+                                pwValid: true
+                            });
+                        } else if (elem.id === "email-field") {
+                            this.setState( {
+                                emailValid: true
+                            })
+                        }
+                        // errorLabel.textContent = "";
+                    }
+                }
+            }
+
+            return false;
+        } else {
+            for (let i = 0; i < formLength; i++) {
+                const elem = form[i];
+                const errorLabel = elem.parentNode.querySelector(".invalid-feedback");
+                if (errorLabel && elem.nodeName.toLowerCase() !== "button") {
+                    errorLabel.textContent = "";
+                }
+            }
+
+            return true;
+        }
+    };
+
+    submitHandler = event => {
+        event.preventDefault();
+
+        if (this.validate()) {
+            this.props.submit();
+        }
+
+        //this.setState({ isValidated: true });
+    };
 
     render() {
         return (
@@ -16,11 +85,21 @@ class Signup extends React.Component {
                 <div className="signup-container">
                     <h1>Sign up</h1>
                     <hr className="hrb"/>
-                    <form>
-                        <EntryField faIcon="user" placeHolder="Username"/>
-                        <EntryField faIcon="envelope" placeHolder="Email"/>
-                        <PwField/>
-                        <button className="submit-btn" type="submit">Sign up</button>
+                    <form id="entry-form">
+                        <EntryField inputId="user-field"
+                                    faIcon="user"
+                                    placeHolder="Username"
+                                    inputPattern="^(?=.*[A-Za-z])[A-Za-z\d\._\-]{1,}$"
+                                    inputValid={this.state.userValid}
+                                    errorMsg="Usernames must contain at least one letter, numbers, hyphens, underscores & periods"/>
+                        <EntryField inputId="email-field"
+                                    faIcon="envelope"
+                                    placeHolder="Email"
+                                    inputType="email"
+                                    inputValid={this.state.emailValid}
+                                    errorMsg="Not a valid email address"/>
+                        <PwField id="pw-field" inputValid={this.state.pwValid}/>
+                        <button className="submit-btn" type="submit" onClick={this.submitHandler}>Sign up</button>
                         <span>
                             Already have an account?
                             <Link className="sign-up" to="/login">Log in</Link>
--- root/frontend/react-src/src/common/EntryComponents.js
+++ root/frontend/react-src/src/common/EntryComponents.js
@@ -15,13 +15,21 @@ export class PwField extends React.Component {
     };
     render() {
         return (
-            <div className="input-container">
-                <FontAwesomeIcon icon="key"/>
-                <input id="pwd-input" className="text-input pw-input" placeholder="Password" type={this.state.pwShow ? "text" : "password"} required/>
-                <button type="button" onClick={this.togglePwShow} id="show-btn">
-                   {/*<FontAwesomeIcon icon={this.state.pwShow ? "eye-slash" : "eye"}/>*/}
-                    {this.state.pwShow ? "HIDE": "SHOW"}
-               </button>
+            <div className={"container-margin"}>
+                <div className="input-container">
+                    <FontAwesomeIcon icon="key"/>
+                    <input className="text-input pw-input" placeholder="Password"
+                           id="pw-field"
+                           type={this.state.pwShow ? "text" : "password"}
+                           minLength={8}
+                           pattern="^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$"
+                           required/>
+                    <button type="button" onClick={this.togglePwShow} id="show-btn">
+                       {/*<FontAwesomeIcon icon={this.state.pwShow ? "eye-slash" : "eye"}/>*/}
+                        {this.state.pwShow ? "HIDE": "SHOW"}
+                   </button>
+                </div>
+                <div className={this.props.inputValid ? "field-valid" : "field-invalid"}>Passwords must be a least 8 characters long and be a mix of letter, digits & symbols</div>
             </div>
         )
     }
@@ -30,9 +38,12 @@ export class PwField extends React.Component {
 export class EntryField extends React.Component {
     render() {
         return (
-            <div className="input-container">
-                <FontAwesomeIcon icon={this.props.faIcon}/>
-                <input className="text-input" placeholder={this.props.placeHolder} required/>
+            <div className="container-margin">
+                <div className="input-container">
+                    <FontAwesomeIcon icon={this.props.faIcon}/>
+                    <input className="text-input" placeholder={this.props.placeHolder} type={this.props.inputType} pattern={this.props.inputPattern} id={this.props.inputId} required/>
+                </div>
+                <div className={this.props.inputValid ? "field-valid" : "field-invalid"}>{this.props.errorMsg}</div>
             </div>
         )
     }
