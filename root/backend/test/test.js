743e19ebc514f094d2c5169ccc0c58060f267e4c
Sean Oh <sean.so.oh@gmail.com>
2018-12-14 14:20:02 -0800

    Create first express test

--- root/backend/test/test.js
+++ root/backend/test/test.js
@@ -1,36 +1,60 @@
 const assert = require('assert')
 const mongoose = require('mongoose')
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const expect = chai.expect
+
+chai.use(chaiHttp)
 
 describe('Database tests', function(){
 
-    // create a test database
+    // connect to database to drop it
     before(function(done){
-        mongoose.connect('mongodb://localhost:27017/testGymmate', function(err){
+        mongoose.connect('mongodb://localhost:27017/gymmate', function(err){
             if(err) throw err
             mongoose.connection.db.dropDatabase(function(err) {
                 if(err) throw err
                 console.log("Dropped existing database")
-                done();
-            });
+                mongoose.connection.close(function(err){
+                    if(err) throw err
+                    console.log("Disconnected from database")
+                    done()
+                })
+            })
         })
         mongoose.connection.on('error', (err) => {
             console.log('Database Error' + err)
         })
         mongoose.connection.on('connected', () => {
-            console.log('Connected to database');
+            console.log('Connected to database')
         })
     })
 
+    // Template test
     describe('Testing test', function() {
         it('should return -1 when the value is not present', function() {
-            assert.equal([1,2,3].indexOf(4), -1);
-        });
+            assert.equal([1,2,3].indexOf(4), -1)
+        })
     })
 
-    after(function(done){
-        mongoose.connection.db.dropDatabase(function(){
-            mongoose.connection.close(done);
-            console.log("Closed connection")
+    // POST /users/
+    describe('Testing POST to /users/', function() {
+
+        // Missing params
+        describe('POST with empty body', function() {
+            it('should fail with code 422', function(done) {
+                chai.request('http://localhost:8080')
+                    .post('/users')
+                    .set('Content-Type', 'application/json')
+                    .send({
+                        // empty body
+                    })
+                    .end(function(err, res){
+                        expect(err).to.be.null
+                        expect(res).to.have.status(422)
+                        done()
+                    })
+            })
         })
     })
 })
