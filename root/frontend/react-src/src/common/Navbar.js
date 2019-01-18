3af4b1223beed8c6a9d8c286d72029c2e7baa224
Sean Oh <sean.so.oh@gmail.com>
2019-01-17 22:16:38 -0800

    Convert React context API to redux

--- root/frontend/react-src/src/common/Navbar.js
+++ root/frontend/react-src/src/common/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { NavLink, Link } from 'react-router-dom';
+import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
+import { connect } from 'react-redux'
 
-import { ModalContext } from './ModalContext';
-import EntryModal from '../auth/EntryContent';
-import NavbarCollapse from './NavbarCollapse';
+import { SIGNUP_MODAL, LOGIN_MODAL } from '../constants/modalTypes'
+import { showModal } from './modalActions'
+import NavbarCollapse from './NavbarCollapse'
 import './Navbar.css';
 import AuthService from '../auth/AuthService'
 
@@ -55,59 +56,54 @@ class Navbar extends React.Component {
 
                     {/* Collapsible navbar items */}
                     <NavbarCollapse in={!this.state.collapsed}>
-                        <ModalContext.Consumer>
-                            { /* grab showModal from ModalContext value */ }
-                            { ({showModal}) =>
-                                <ul className="navbar-nav">
+                        <ul className="navbar-nav">
 
-                                    <li className="nav-item" onClick={this.collapseNavbar}>
-                                        <NavLink className="nav-link" exact to="/">Home</NavLink>
-                                    </li>
+                            <li className="nav-item" onClick={this.collapseNavbar}>
+                                <NavLink className="nav-link" exact to="/">Home</NavLink>
+                            </li>
 
-                                    <li className="nav-item" onClick={this.collapseNavbar}>
-                                        <NavLink className="nav-link" to="/programs">Programs</NavLink>
-                                    </li>
+                            <li className="nav-item" onClick={this.collapseNavbar}>
+                                <NavLink className="nav-link" to="/programs">Programs</NavLink>
+                            </li>
 
-                                    <li className="nav-item" onClick={this.collapseNavbar}>
-                                        <NavLink className="nav-link" to="/exercises">Exercises</NavLink>
-                                    </li>
+                            <li className="nav-item" onClick={this.collapseNavbar}>
+                                <NavLink className="nav-link" to="/exercises">Exercises</NavLink>
+                            </li>
 
-                                    <li className="nav-item" onClick={this.collapseNavbar}>
-                                        <NavLink className="nav-link" to="/profile">Profile</NavLink>
-                                    </li>
-                                    {!this.state.isLoggedIn ? (
-                                    <>
-                                        <li className="nav-item" onClick={this.collapseNavbar}>
-                                            <button type="button"
-                                                className="link-btn nav-link"
-                                                onClick={() => showModal(EntryModal, {isSignup: true})}
-                                            >
-                                                Sign up
-                                            </button>
-                                        </li>
+                            <li className="nav-item" onClick={this.collapseNavbar}>
+                                <NavLink className="nav-link" to="/profile">Profile</NavLink>
+                            </li>
+                            {!this.state.isLoggedIn ? (
+                            <>
+                                <li className="nav-item" onClick={this.collapseNavbar}>
+                                    <button type="button"
+                                        className="link-btn nav-link"
+                                        onClick={this.props.openSignup}
+                                    >
+                                        Sign up
+                                    </button>
+                                </li>
 
-                                        <li className="nav-item" onClick={this.collapseNavbar}>
-                                            <button type="button"
-                                                className="link-btn nav-link"
-                                                onClick={() => showModal(EntryModal, {isSignup: false})}
-                                            >
-                                                Log in
-                                            </button>
-                                        </li>
-                                    </>
-                                    ) : (
-                                        <li className="nav-item" onClick={this.collapseNavbar}>
-                                            <button type="button"
-                                                className="link-btn nav-link"
-                                                onClick={this.logout}
-                                            >
-                                                Log out
-                                            </button>
-                                        </li>
-                                    )}
-                                </ul>
-                            }
-                        </ModalContext.Consumer>
+                                <li className="nav-item" onClick={this.collapseNavbar}>
+                                    <button type="button"
+                                        className="link-btn nav-link"
+                                        onClick={this.props.openLogin}
+                                    >
+                                        Log in
+                                    </button>
+                                </li>
+                            </>
+                            ) : (
+                                <li className="nav-item" onClick={this.collapseNavbar}>
+                                    <button type="button"
+                                        className="link-btn nav-link"
+                                        onClick={this.logout}
+                                    >
+                                        Log out
+                                    </button>
+                                </li>
+                            )}
+                        </ul>
                     </NavbarCollapse>
                 </div>
             </nav>
@@ -115,6 +111,18 @@ class Navbar extends React.Component {
     }
 }
 
-Navbar.defaultProps = defaultProps;
+const mapDispatchToProps = dispatch => {
+    return {
+        openSignup: () => {
+            dispatch(showModal(SIGNUP_MODAL))
+        },
+        openLogin: () => {
+            dispatch(showModal(LOGIN_MODAL))
+        }
+    }
+}
 
-export default Navbar;
+Navbar.defaultProps = defaultProps
+Navbar = connect(null, mapDispatchToProps)(Navbar)
+
+export default Navbar
---
+++ root/frontend/react-src/src/common/modalReducer.js
@@ -0,0 +1,24 @@
+import { SHOW_MODAL, HIDE_MODAL } from '../constants/actionTypes'
+
+const initialState = {
+    modalType: null,
+    modalProps: {},
+    modalOpen: false,
+}
+
+export default function app(state = initialState, action) {
+    switch (action.type) {
+        case SHOW_MODAL:
+            return Object.assign({}, state, {
+                modalType: action.modalType,
+                modalProps: action.modalProps,
+                modalOpen: true,
+            })
+        case HIDE_MODAL:
+            return Object.assign({}, state, {
+                modalOpen: false,
+            })
+        default:
+            return state
+    }
+}
