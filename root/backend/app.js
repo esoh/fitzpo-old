d695d5824b86bde133c354e5e37fab069f6b0eb6
Sean Oh <sean.so.oh@gmail.com>
2019-05-10 13:21:22 -0700

    feat(sql): Sync models before starting server

--- root/backend/app.js
+++ root/backend/app.js
@@ -1,15 +1,32 @@
 //3rd party modules
 const express = require('express')
+const http = require('http')
+
+const env = process.env.NODE_ENV || 'dev'
+const config = require('./config/config.js')[env]
 
 const models = require('./models')
-const env = process.env.NODE_ENV || 'dev';
-const config = require('./config/config.js')[env]
 const routes = require('./routes')
+
+// set up express app
 
 const app = express()
 
 app.use('/', routes)
 
-app.listen(config.app.port, () => {
-    console.log('Server started on port ' + config.app.port)
-})
+
+
+
+// start the server
+// maybe move this to ./bin/www?
+//
+// sync models that have not been synced yet, then start the server
+models.sequelize.sync()
+    .then(() => {
+        app.listen(config.app.port, () => {
+            console.log('Server listening on port ' + config.app.port)
+        })
+    })
+    .catch(err => { throw err })
+
+
