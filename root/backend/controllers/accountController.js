0c71961170d31738e0fb4e63a3642260d1a5c864
Sean Oh <sean.so.oh@gmail.com>
2019-05-10 22:54:27 -0700

    refactor(account): abstract model functions to model

--- root/backend/controllers/accountController.js
+++ root/backend/controllers/accountController.js
@@ -2,7 +2,7 @@ const model = require('../models')
 
 module.exports = {
     list: function(req, res){
-        model.Account.findAll()
+        model.Account.list()
             .then(accounts => {
                 // this is where you would generate a view
                 res.send(accounts)
--- root/backend/models/account.js
+++ root/backend/models/account.js
@@ -1,12 +1,22 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
-  const Account = sequelize.define('Account', {
-    username: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING
-  }, {});
-  Account.associate = function(models) {
-    // associations can be defined here
-  };
-  return Account;
-};
+    const Account = sequelize.define('Account', {
+        username: DataTypes.STRING,
+        email: DataTypes.STRING,
+        password: DataTypes.STRING
+    }, {})
+    Account.associate = function(models) {
+        // associations can be defined here
+    }
+
+    // list accounts
+    // limit: up to <limit> accounts shown
+    Account.list = function(limit=20) {
+        return new Promise((resolve, reject) => {
+            Account.findAll({ limit: limit })
+                .then(accounts => { resolve(accounts) })
+                .catch(err => { reject(err) })
+        })
+    }
+    return Account
+}
