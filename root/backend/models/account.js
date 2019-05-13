981311b53ca3b652a9d81c95cc8a7c8b551f9d62
Sean Oh <sean.so.oh@gmail.com>
2019-05-13 12:13:49 -0700

    feat(accounts): Define validations for account object

--- root/backend/models/account.js
+++ root/backend/models/account.js
@@ -1,13 +1,40 @@
 'use strict'
 const bcrypt = require('bcrypt')
+const Filter = require('bad-words')
 
+const filter = new Filter();
 const SALT_ROUNDS = 10
 
 module.exports = (sequelize, DataTypes) => {
     const Account = sequelize.define('Account', {
-        username: DataTypes.STRING,
-        email: DataTypes.STRING,
-        password: DataTypes.STRING
+        username: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,// TODO: split into username_display and username_id = lower(username_display)
+            validate: {
+                is: /^(?=.*[A-Za-z])[A-Za-z0-9d._-]{1,}$/,
+                isNotProfane(value) {
+                    if(filter.isProfane(value)) {
+                        throw new Error('Inappropriate Username')
+                    }
+                }
+            }
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                is: /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$.!%*#?&])[A-Za-z\d@$!.%*#?&]{8,}/
+            }
+        }
     }, {
         hooks: {
             beforeCreate: (account) => {
