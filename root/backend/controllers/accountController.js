ff12b2337169ac8bd6b7463e7685f4d9c52c86ee
Sean Oh <sean.so.oh@gmail.com>
2019-05-15 22:35:52 -0700

    refactor(account): factor out database error catcher for future code reuse

--- root/backend/controllers/accountController.js
+++ root/backend/controllers/accountController.js
@@ -1,6 +1,55 @@
 const {Account} = require('../models')
 const APIError = require('../utils/errorBuilder').APIError
 
+catchSchemaErrors = function(err) {
+    let details = []
+    let params = []
+    var error = null
+    switch(err.name){
+        case 'ValidationError':
+            for (var i in err.errors){
+                details.push(err.errors[i].details)
+            }
+            error = new APIError({
+                title: 'Input validation constraints error',
+                detail: 'The following messages for the violating fields are outputted below:\n' + details.join('\n')
+            })
+            return {
+                error,
+                statusCode: 400
+            }
+            break
+        case 'UniqueConstraintError':
+            for (var i in err.errors){
+                params.push(err.errors[i].param)
+            }
+            error = new APIError({
+                title: 'Unique constraint error',
+                detail: 'The inputs for the following fields must be unique: ' + params.join(", ")
+            })
+            return {
+                error,
+                statusCode: 409
+            }
+            break
+        case 'NotNullConstraintError':
+            for (var i in err.errors){
+                params.push(err.errors[i].param)
+            }
+            error = new APIError({
+                title: 'Not null constraint error',
+                detail: 'The inputs for the following fields are required: ' + params.join(", ")
+            })
+            return {
+                error,
+                statusCode: 400
+            }
+            break
+        default:
+            return null
+    }
+}
+
 module.exports = {
     list: function(req, res, next){
         // controller calls the method function
@@ -25,37 +74,9 @@ module.exports = {
                 //handle error thrown by model in controller. Business logic.
                 let details = []
                 let params = []
-                switch(err.name){
-                    case 'ValidationError':
-                        for (var i in err.errors){
-                            details.push(err.errors[i].details)
-                        }
-                        res.status(400).send(new APIError({
-                            title: 'Input validation constraints error',
-                            detail: 'The following messages for the violating fields are outputted below:\n' + details.join('\n')
-                        }))
-                        break
-                    case 'UniqueConstraintError':
-                        for (var i in err.errors){
-                            params.push(err.errors[i].param)
-                        }
-                        res.status(409).send(new APIError({
-                            title: 'Unique constraint error',
-                            detail: 'The inputs for the following fields must be unique: ' + params.join(", ")
-                        }))
-                        break
-                    case 'NotNullConstraintError':
-                        for (var i in err.errors){
-                            params.push(err.errors[i].param)
-                        }
-                        res.status(400).send(new APIError({
-                            title: 'Not null constraint error',
-                            detail: 'The inputs for the following fields are required: ' + params.join(", ")
-                        }))
-                        break
-                    default:
-                        next(err)
-                }
+                schemaErr = catchSchemaErrors(err)
+                if(schemaErr) return res.status(schemaErr.statusCode).send(schemaErr.error)
+                next(err)
             })
     }
 }
