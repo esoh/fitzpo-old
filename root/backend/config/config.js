d615b020d873cfb1ef711488dcb9151001b5e629
Sean Oh <sean.so.oh@gmail.com>
2019-05-18 17:40:26 -0700

    chore(config): Add environmental variable to control database logging

--- root/backend/config/config.js
+++ root/backend/config/config.js
@@ -2,14 +2,24 @@ const fs = require('fs');
 // import from .env
 require('dotenv').config({path:__dirname+'/../.env'})
 
+// need to set boolean vars using alt method because of bool(false) == bool(null)
+setWithStrBool = function(str, alt){
+    if(str == 'false'){
+        return false
+    } else {
+        return alt
+    }
+}
+
 defaults = {
     // used by sequelize
     username:       process.env.DB_USERNAME         || 'root',
     password:       process.env.DB_PASSWORD         || 'password',
     database:       process.env.DB_NAME             || 'fitzpo',
-    host:           process.env.DB_HOST             ||  'localhost',
-    port:           parseInt(process.env.DB_PORT)   ||  3306,
-    dialect:        process.env.DB_DIALECT          ||  'mysql',
+    host:           process.env.DB_HOST             || 'localhost',
+    port:           parseInt(process.env.DB_PORT)   || 3306,
+    dialect:        process.env.DB_DIALECT          || 'mysql',
+    logging:        setWithStrBool(process.env.DB_LOGGING, console.log),
 
     // define your own
     app_port:       parseInt(process.env.APP_PORT)  ||  8080,
@@ -30,6 +40,7 @@ module.exports = {
         host:           process.env.DEV_DB_HOST             ||  defaults.host,
         port:           parseInt(process.env.DEV_DB_PORT)   ||  defaults.port,
         dialect:        process.env.DEV_DB_DIALECT          ||  defaults.dialect,
+        logging:        setWithStrBool(process.env.DEV_DB_LOGGING, defaults.logging),
 
         app_port:       parseInt(process.env.DEV_APP_PORT)  ||  defaults.app_port,
         auth_secret:    process.env.DEV_AUTH_SECRET         ||  defaults.auth_secret,
@@ -42,6 +53,7 @@ module.exports = {
         host:           process.env.TEST_DB_HOST             ||  defaults.host,
         port:           parseInt(process.env.TEST_DB_PORT)   ||  defaults.port,
         dialect:        process.env.TEST_DB_DIALECT          ||  defaults.dialect,
+        logging:        setWithStrBool(process.env.TEST_DB_LOGGING, defaults.logging),
 
         app_port:       parseInt(process.env.TEST_APP_PORT)  ||  defaults.app_port,
         auth_secret:    process.env.TEST_AUTH_SECRET         ||  defaults.auth_secret,
@@ -54,6 +66,7 @@ module.exports = {
         host:           process.env.PROD_DB_HOST,
         port:           parseInt(process.env.PROD_DB_PORT),
         dialect:        process.env.PROD_DB_DIALECT,
+        logging:        false,
 
         app_port:       parseInt(process.env.PROD_APP_PORT),
         auth_secret:    process.env.PROD_AUTH_SECRET,
