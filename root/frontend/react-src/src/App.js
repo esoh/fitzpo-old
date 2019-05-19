a7ed0a46ddbb704f613380f81913db09a3a3a3b3
Sean Oh <sean.so.oh@gmail.com>
2019-05-18 21:29:58 -0700

    feat(webapp): Create home page with navigation links to register and login

--- root/frontend/react-src/src/App.js
+++ root/frontend/react-src/src/App.js
@@ -4,11 +4,13 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Register from './auth/Register';
 import Login from './auth/Login';
+import Home from './home/Home';
 
 function App() {
     return (
         <Router>
-            <Route path='/register' component={Register} />
+            <Route path='/' exact component={Home} />
+            <Route path='/signup' component={Register} />
             <Route path='/login' component={Login} />
         </Router>
     );
