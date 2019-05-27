bab76578f0ce577b3cc7a88fdafcb8fa9df010da
Sean Oh <sean.so.oh@gmail.com>
2019-05-27 00:23:18 -0700

    test(travis): Add support for travis-ci

--- root/backend/config/config.js
+++ root/backend/config/config.js
@@ -14,7 +14,7 @@ setWithStrBool = function(str, alt){
 defaults = {
     // used by sequelize
     username:       process.env.DB_USERNAME         || 'root',
-    password:       process.env.DB_PASSWORD         || 'password',
+    password:       process.env.DB_PASSWORD,
     database:       process.env.DB_NAME             || 'fitzpo',
     host:           process.env.DB_HOST             || 'localhost',
     port:           parseInt(process.env.DB_PORT)   || 3306,
