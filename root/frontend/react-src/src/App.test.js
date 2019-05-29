438178c407dcd83a545d69b8e7597eb3f0b1d7d0
Sean Oh <sean.so.oh@gmail.com>
2019-05-28 18:07:11 -0700

    test(App): Add app tests to confirm function calls to API handler

--- root/frontend/react-src/src/App.test.js
+++ root/frontend/react-src/src/App.test.js
@@ -1,3 +1,5 @@
+jest.mock('./services/authService');
+
 import React from 'react';
 import { shallow } from 'enzyme';
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -7,9 +9,13 @@ import Signup from './entry/Signup';
 import Login from './entry/Login';
 import Home from './home/Home';
 
+import { checkLoggedIn } from './services/authService';
+
+const setLoggedIn = jest.fn();
+
 function setup(){
     const props = {
-        setLoggedIn: jest.fn()
+        setLoggedIn,
     };
 
     const wrapper = shallow(<App {...props}/>);
@@ -21,8 +27,17 @@ function setup(){
 }
 
 describe('App component', () => {
+
+    afterEach(() => {
+        checkLoggedIn.mockClear();
+        setLoggedIn.mockClear();
+    })
+
     it('Should render router successfully', () => {
+        checkLoggedIn.mockReturnValue(Promise.resolve());
+
         const { wrapper } = setup();
+
         expect(wrapper.find(Router).length).toBe(1);
 
         const pathMap = wrapper.find(Route).reduce((pathMap, route) => {
@@ -33,5 +48,39 @@ describe('App component', () => {
         expect(pathMap['/']).toBe(Home);
         expect(pathMap['/signup']).toBe(Signup);
         expect(pathMap['/login']).toBe(Login);
+    })
+
+    it('Should call mocked checkLoggedIn which returns a valid account then call setLoggedIn with param true', () => {
+        const promise = Promise.resolve({ account: { username: 'test_username' } })
+        checkLoggedIn.mockReturnValue(promise);
+
+        expect(checkLoggedIn.mock.calls.length).toBe(0);
+        expect(setLoggedIn.mock.calls.length).toBe(0);
+        const { wrapper } = setup();
+        expect(checkLoggedIn.mock.calls.length).toBe(1);
+
+        return promise.then(() => {
+            wrapper.update();
+        }).then(() => {
+            expect(setLoggedIn.mock.calls.length).toBe(1);
+            expect(setLoggedIn.mock.calls[0][0]).toBe(true);
+        })
+    })
+
+    it('Should call mocked checkLoggedIn which returns an empty response to call setLoggedIn with param false', () => {
+        const promise = Promise.resolve(null)
+        checkLoggedIn.mockReturnValue(promise);
+
+        expect(checkLoggedIn.mock.calls.length).toBe(0);
+        expect(setLoggedIn.mock.calls.length).toBe(0);
+        const { wrapper } = setup();
+        expect(checkLoggedIn.mock.calls.length).toBe(1);
+
+        return promise.then(() => {
+            wrapper.update();
+        }).then(() => {
+            expect(setLoggedIn.mock.calls.length).toBe(1);
+            expect(setLoggedIn.mock.calls[0][0]).toBe(false);
+        })
     })
 })
