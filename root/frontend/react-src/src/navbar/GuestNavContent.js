fdb668d2a990b64657f581dfc0756416c084f55e
Sean Oh <sean.so.oh@gmail.com>
2019-06-20 16:08:26 -0700

    feat(nav): Add Home link to navbar

--- root/frontend/react-src/src/navbar/GuestNavContent.js
+++ root/frontend/react-src/src/navbar/GuestNavContent.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function GuestNavContent(){
     return (
         <div>
+            <Link to="/">Home</Link>
             <Link to="/login">Log In</Link>
             <Link to="/signup">Sign Up</Link>
         </div>
--- root/frontend/react-src/src/navbar/UserNavContent.js
+++ root/frontend/react-src/src/navbar/UserNavContent.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 export default function UserNavContent(props){
     return (
         <div>
+            <Link to='/'>Home</Link>
             <Link to='/exercise-logs'>My Logs</Link>
         </div>
     )
