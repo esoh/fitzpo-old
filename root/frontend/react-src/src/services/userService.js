ee37ac2b84b9a7424d853e2da3aaa8743f808fa0
Sean Oh <sean.so.oh@gmail.com>
2019-06-19 20:12:22 -0700

    style(service): Remove console log when deleting exercise

--- root/frontend/react-src/src/services/userService.js
+++ root/frontend/react-src/src/services/userService.js
@@ -20,7 +20,6 @@ function getUserExerciseLogs(){
 function deleteExerciseLog(id){
     // have to make sure user is authorized to do this
     // check if user extracted from cookie is owner of this exercise log
-    console.log("DELETING EXERCISE " + id);
     return fetch('/user/exercise-logs/' + id, { method: 'DELETE' });
 }
 
