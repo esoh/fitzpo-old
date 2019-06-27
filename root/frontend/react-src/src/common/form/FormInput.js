0782c3412b1eb3025b76615fd284109399b122ac
Sean Oh <sean.so.oh@gmail.com>
2019-06-26 21:01:01 -0700

    style(display): Clean up form code

--- root/frontend/react-src/src/common/form/FormInput.js
+++ root/frontend/react-src/src/common/form/FormInput.js
@@ -38,21 +38,15 @@ class FormInput extends React.Component{
         if(errors) errorMessages = errors.map(msg => <p className={errorClassName} key={msg}>{msg}</p>);
         let inputClassName = isError ? errorClassName : '';
 
-        var helperMessages = null;
-        if(this.state.showHelper){
-            let messages = helper.map(msg => <p key={msg}>{msg}</p>);
-            helperMessages = (
-                <div className={helperClassName} for={inputAttr.name}>
-                    {messages}
-                </div>
-            )
-        }
-
         return (
             <label>
                 {label}
                 <input id={inputAttr.name} {...inputAttr} className={inputClassName} onFocus={this.onFocus} onBlur={this.onBlur}/>
-                {helperMessages}
+                {(this.state.showHelper) ? (
+                    <div className={helperClassName} for={inputAttr.name}>
+                        {helper.map(msg => (<p key={msg}>{msg}</p>))}
+                    </div>
+                ) : null}
                 {errorMessages}
             </label>
         );
