8e8cae80d2e1774a5a21ec2131732663bff0dc8b
Sean Oh <sean.so.oh@gmail.com>
2019-06-26 23:23:01 -0700

    feat(tooltip): Move hint tooltip to right side of input field

--- root/frontend/react-src/src/common/popper/Tooltip.js
+++ root/frontend/react-src/src/common/popper/Tooltip.js
@@ -4,17 +4,28 @@ import PropTypes from 'prop-types';
 import styles from './Tooltip.module.scss';
 
 class Tooltip extends React.Component{
+
     render() {
 
         let {
             visible,
+            targetRect,
             ...attributes
         } = this.props;
 
         if(!visible) return null;
 
+        let position = {
+            left: targetRect.right,
+            top: targetRect.top,
+        }
+
         return (
-            <div {...attributes} className={styles.tooltip}/>
+            <div
+                {...attributes}
+                className={styles.tooltip}
+                style={position}
+            />
         );
     }
 }
